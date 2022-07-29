97ced1ac3e1bec1c2993096c0ee646e3dc4f5157
Carlos Rene <carlos.tr00@gmail.com>
2022-07-29 11:55:09 -0300

    :zap: format text

--- java/BEE1175.java
+++ java/BEE1175.java
@@ -1,7 +1,9 @@
 /* 
     BEECROWD 1175 | Troca em Vetor I
 
-    Faça um programa que leia um vetor N[20]. Troque a seguir, o primeiro elemento com o último, o segundo elemento com o penúltimo, etc., até trocar o 10º com o 11º. Mostre o vetor modificado.
+    Faça um programa que leia um vetor N[20]. Troque a seguir, 
+    o primeiro elemento com o último, o segundo elemento com o penúltimo, etc., 
+    até trocar o 10º com o 11º. Mostre o vetor modificado.
 
     Entrada:
         A entrada contém 20 valores inteiros, positivos ou negativos.
